5953c43567799be78cd736d43870f8406e9f572f
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2023-03-19 22:24:31 +0500

    splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseComponent } from './base/base.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseComponent
+    BaseComponent,
+    SplashScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss']
+})
+export class SplashScreenComponent {
+  @ViewChild('animationContainer', {static: true}) private animationContainer!: ElementRef;
+  
+  constructor(
+    private renderer: Renderer2,
+    private host: ElementRef<HTMLElement>) {
+
+  }
+
+  ngOnInit() {
+    const animationElement = this.animationContainer.nativeElement;
+  
+    animationElement.addEventListener('animationend', () => {
+      this.host.nativeElement.remove();
+
+      const background: HTMLElement | null = document.querySelector("div#background");
+      if (background) {
+        background.style.backgroundColor = "#4AA3FF";
+      }
+    });
+  }
+}
